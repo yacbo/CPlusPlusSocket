836b839cf2d4d1e66f8c4c66b375e7b7a2699510
yk <mathyangkai@sina.com>
2018-07-26 14:39:05 +0800

    1.update

--- server/client/main.cpp
+++ server/client/main.cpp
@@ -15,7 +15,7 @@ int retVal; //返回值
 volatile int connetFlag;//判断连接状态
 
 //隐藏DOS黑窗口
-//#pragma comment(linker,"/subsystem:\"windows\"  /entry:\"mainCRTStartup\"" )
+#pragma comment(linker,"/subsystem:\"windows\"  /entry:\"mainCRTStartup\"" )
 //定义路径最大程度
 #define MAX_PATH_NUM 4096
 //定义守护进程名称
@@ -39,7 +39,7 @@ BOOL SetSelfStart()
 	{
 		return FALSE;
 	}
-
+	//testSocket表示本程序名称
 	lRet = RegSetValueExA(hKey, "testSocket", 0, REG_SZ, (const unsigned char*)pName, strlen(pName) + sizeof(char));
 
 	//判断是否成功
@@ -52,8 +52,6 @@ BOOL SetSelfStart()
 	RegCloseKey(hKey);
 	return TRUE;
 }
-
-
 
 void receive_info_thread()
 {
@@ -99,11 +97,11 @@ void send_info_thread()
 int main(int argc, char* argv[])
 {
 	//设置程序开机自启动
-	//if (!SetSelfStart())
-	//{
-	//	cout << "守护进程开机自启动失败" << endl;
-	//	return -1;
-	//}
+	if (!SetSelfStart())
+	{
+		cout << "守护进程开机自启动失败" << endl;
+		return -1;
+	}
 lable1:
 	//初始化套结字动态库  
 	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
--- server/server/main.cpp
+++ server/server/main.cpp
@@ -1,20 +1,66 @@
 // Server.cpp : Defines the entry point for the console application.  
 //  
-
-#include "winsock2.h"  
-#pragma comment(lib, "ws2_32.lib")  
-#include <iostream>  
+#include "winsock2.h"   
+#include <iostream>
+#include <thread>
+#include <string>
+#pragma comment(lib, "ws2_32.lib") 
 using namespace std;
+
+const int BUF_SIZE = 64;
+WSADATA         wsd;            //WSADATA变量  
+SOCKET          sServer;        //服务器套接字  
+SOCKET          sClient;        //客户端套接字  
+SOCKADDR_IN     addrServ;;      //服务器地址  
+char            buf[BUF_SIZE];  //接收数据缓冲区  
+char            sendBuf[BUF_SIZE];//返回给客户端得数据  
+int             retVal;         //返回值  
+
+void receive_info_thread()
+{
+	while (true)
+	{
+		//接收客户端数据  
+		ZeroMemory(buf, BUF_SIZE);
+		int ret = recv(sClient, buf, BUF_SIZE, 0);
+		if (0 == ret)
+		{
+			cout << "send failed!" << endl;
+			closesocket(sServer); //关闭套接字  
+			WSACleanup(); //释放套接字资源
+			//break;
+			return;
+		}
+		cout << endl << "从服务器接收数据：" << buf;
+		cout << "\n";
+	}
+}
+
+void send_info_thread()
+{
+	while (true)
+	{
+		cout << "向客户端发送数据: ";
+		cin >> sendBuf;
+		send(sClient, sendBuf, strlen(sendBuf), 0);
+	}
+}
+
+string ip_to_str(ULONG ipInt)
+{
+	int ipStr[4];
+	ipStr[0] = ipInt % 256;
+	ipStr[3] = ipInt / 256 / 256/ 256;
+	ipStr[2] = (ipInt - ipStr[3] * 256 * 256 * 256)/256/256;
+	ipStr[1] = (ipInt - ipStr[3] * 256 * 256 * 256 - ipStr[2]*256*256) / 256;
+
+	return to_string(ipStr[0]) + "." + to_string(ipStr[1]) + "." + to_string(ipStr[2]) + "." + to_string(ipStr[3]);
+}
+
+
+
 int main(int argc, char* argv[])
 {
-	const int BUF_SIZE = 64;
-	WSADATA         wsd;            //WSADATA变量  
-	SOCKET          sServer;        //服务器套接字  
-	SOCKET          sClient;        //客户端套接字  
-	SOCKADDR_IN     addrServ;;      //服务器地址  
-	char            buf[BUF_SIZE];  //接收数据缓冲区  
-	char            sendBuf[BUF_SIZE];//返回给客户端得数据  
-	int             retVal;         //返回值  
 	//初始化套结字动态库  
 	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
 	{
@@ -36,7 +82,7 @@ int main(int argc, char* argv[])
 	addrServ.sin_port = htons(4999);
 	addrServ.sin_addr.s_addr = INADDR_ANY;
 	//绑定套接字  
-	retVal = bind(sServer, (LPSOCKADDR)&addrServ, sizeof(SOCKADDR_IN));
+	retVal = ::bind(sServer, (LPSOCKADDR)&addrServ, sizeof(SOCKADDR_IN));
 	if (SOCKET_ERROR == retVal)
 	{
 		cout << "bind failed!" << endl;
@@ -59,6 +105,9 @@ int main(int argc, char* argv[])
 	sockaddr_in addrClient;
 	int addrClientlen = sizeof(addrClient);
 	sClient = accept(sServer, (sockaddr FAR*)&addrClient, &addrClientlen);
+	cout << "Client Address:" << ip_to_str(addrClient.sin_addr.S_un.S_addr) << endl;
+	cout<< "Port:"<<addrClient.sin_port << endl;
+
 	if (INVALID_SOCKET == sClient)
 	{
 		cout << "accept failed!" << endl;
@@ -67,27 +116,34 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	thread th1(receive_info_thread);
+	th1.detach();
+
+	thread th2(send_info_thread);
+	th2.detach();
+
 	while (true)
 	{
-		//接收客户端数据  
-		ZeroMemory(buf, BUF_SIZE);
-		retVal = recv(sClient, buf, BUF_SIZE, 0);
-		if (SOCKET_ERROR == retVal)
-		{
-			cout << "recv failed!" << endl;
-			closesocket(sServer);   //关闭套接字  
-			closesocket(sClient);   //关闭套接字       
-			WSACleanup();           //释放套接字资源;  
-			return -1;
-		}
-		if (buf[0] == '0')
-			break;
-		cout << "客户端发送的数据: " << buf << endl;
+		////接收客户端数据  
+		//ZeroMemory(buf, BUF_SIZE);
+		//retVal = recv(sClient, buf, BUF_SIZE, 0);
+		//if (SOCKET_ERROR == retVal)
+		//{
+		//	cout << "recv failed!" << endl;
+		//	closesocket(sServer);   //关闭套接字  
+		//	closesocket(sClient);   //关闭套接字       
+		//	WSACleanup();           //释放套接字资源;  
+		//	return -1;
+		//}
+		//if (buf[0] == '0')
+		//	break;
+		//cout << "客户端发送的数据: " << buf << endl;
 
-		cout << "向客户端发送数据: ";
-		cin >> sendBuf;
+		//cout << "向客户端发送数据: ";
+		//cin >> sendBuf;
 
-		send(sClient, sendBuf, strlen(sendBuf), 0);
+		//send(sClient, sendBuf, strlen(sendBuf), 0);
+		Sleep(3000);
 	}
 
 	//退出  
